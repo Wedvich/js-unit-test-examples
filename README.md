45a826a79f737c6078b67df9f526a89a183223db
Martin Wedvich <wedvich@gmail.com>
2017-01-19 21:30:09 +0100

    Added ava

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # js-unit-test-examples
-A collection of example setups for common JavaScript unit testing frameworks and tools
+This repository contains a collection of example setups for common JavaScript unit testing frameworks and tools.
+
+|Example|Tests|Coverage|ES6|TypeScript|
+|---|---|---|---|---|
+|[ava](https://github.com/Wedvich/js-unit-test-examples/tree/master/ava)|:heavy_check_mark:||||
+|[jasmine](https://github.com/Wedvich/js-unit-test-examples/tree/master/jasmine)|:heavy_check_mark:||||
+|[mocha-chai](https://github.com/Wedvich/js-unit-test-examples/tree/master/mocha-chai)|:heavy_check_mark:||||
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-module.exports = function () {
-  this.add = function (a, b) {
-    return a + b;
-  };
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.returnExports = factory();
+  }
+}(this, function () {
+  return function () {
+    this.add = function (a, b) {
+      return a + b;
+    };
 
-  this.multiply = function (a, b) {
-    return a * b;
-  };
+    this.multiply = function (a, b) {
+      return a * b;
+    };
 
-  this.fibonacci = function (length) {
-    var sequence = [0, 1];
-    for (var i = 2; i <= length; ++i) {
-      sequence[i] = sequence[i - 1] + sequence[i - 2];
-    }
-    return sequence;
+    this.fibonacci = function (length) {
+      var sequence = [0, 1];
+      for (var i = 2; i <= length; ++i) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+      }
+      return sequence;
+    };
   };
-};
+}));
